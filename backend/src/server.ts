d8adba429b5d84f640cc86591d081558d6d5ba61
yuri-costa-flooxai <yurircostawork@gmail.com>
2025-05-20 03:37:58 -0300

    fix: cors config for development purposes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,7 +5,12 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+}))
 app.listen(3001, '0.0.0.0', () => {
     console.log('Server running on port 0.0.0.0:3001');
 })
