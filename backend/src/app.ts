eaceacef309ff94f5808cd4570906cfc1ea3874b
yuri-costa-flooxai <yurircostawork@gmail.com>
2025-05-20 03:45:21 -0300

    fix: cors file changing
    loading cors at the very beggining

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,15 @@
 import { TaskRouter } from './features/task/routers/TaskRouter';
 import { UserRouter } from './features/user/routers/UserRouter'
 import express from 'express'
+import cors from 'cors';
 
 const app = express();
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+}))
 app.use(express.json());
 
 app.use('/api/users', UserRouter);
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,9 @@
-import cors from 'cors';
 import app from './app';
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-}))
 app.listen(3001, '0.0.0.0', () => {
     console.log('Server running on port 0.0.0.0:3001');
 })
